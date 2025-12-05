b43f0be4afa800cad99d86618ab9053300ff042b
Guilherme Bustamante <guilherme_bustamante@live.com>
2025-12-05 10:26:01 -0300

    chore: add regions and user not found error code

--- src/common/errors/messages/tasks.errorCodes.ts
+++ src/common/errors/messages/tasks.errorCodes.ts
@@ -4,25 +4,38 @@ import { HttpStatus } from '@nestjs/common';
 
 //#region Codes
 export const ErrorCodes = {
+  //#region NOT FOUND
   TASK_NOT_FOUND: {
     code: 'TASK_NOT_FOUND',
     message: 'Esta tarefa não foi encontrada.',
     status: HttpStatus.NOT_FOUND,
   },
+  USER_NOT_FOUND: {
+    code: 'USER_NOT_FOUND',
+    message: 'Este usuário não foi encontrado.',
+    status: HttpStatus.NOT_FOUND,
+  },
+  //#endregion
+  //#region PAYLOAD
   INVALID_PAYLOAD: {
     code: 'INVALID_PAYLOAD',
     message: 'Os dados enviados são inválidos.',
     status: HttpStatus.BAD_REQUEST,
   },
+  //#endregion
+  //#region DATABASE
   DATABASE_ERROR: {
     code: 'DATABASE_ERROR',
     message: 'Erro ao acessar o banco de dados.',
     status: HttpStatus.INTERNAL_SERVER_ERROR,
   },
+  //#endregion
+  //#region SERVER
   INTERNAL_ERROR: {
     code: 'INTERNAL_ERROR',
     message: 'Ocorreu um erro inesperado. Tente novamente.',
     status: HttpStatus.INTERNAL_SERVER_ERROR,
   },
+  //#endregion
 };
 //#endregion
